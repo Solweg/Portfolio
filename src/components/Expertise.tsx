cf859ab7daac81d07431d08ce6983519549adc2c
Hélène Gouguenheim <lngouguenheim@hotmail.fr>
2025-06-03 21:58:30 +0200

    mise à jours compétences

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -57,12 +57,12 @@ function Expertise() {
                 <div className="Accroche-container">
                     <p className="Accroche">Chats, café et code : ma recette pour relever tous les défis. Étudiante DevOps Full Stack à l'EPSI Rennes, je recherche une alternance pour septembre 2025, au rythme de 2 semaines en entreprise / 1 semaine en cours.</p>
                 </div>
-                <h1>Compétences</h1>
+                <h1 className="title">Compétences</h1>
                 <div className="skills-grid">
                     <div className="skill">
                         <FontAwesomeIcon icon={faReact} size="3x" />
-                        <h3>Développement Front-end</h3>
-                        <p>En tant que Développeuse web, j'ai une expertise en développement front-end avec un accent particulier sur l’<strong>accessibilité</strong>. Je transforme des maquettes en interfaces réactives en utilisant des technologies comme <strong>HTML5</strong>, <strong>CSS3</strong>, <strong>JavaScript</strong>, et <strong>React</strong>. J'assure la compatibilité des sites sur tous les appareils tout en respectant les bonnes pratiques d'accessibilité (WCAG).</p>
+                        <h3>Création de sites web modernes et accessibles</h3>
+                        <p>Je conçois des sites et des applications web qui s’adaptent à tous les écrans : smartphones, tablettes, ordinateurs… L’objectif est que chacun puisse naviguer facilement, quel que soit son appareil ou ses capacités. Je transforme des maquettes graphiques en pages web interactives, en veillant à ce que le site soit fluide, rapide et agréable à utiliser pour tout le monde.</p>
                         <div className="flex-chips">
                             <span className="chip-title">Technologies:</span>
                             {labelsFirst.map((label, index) => (
@@ -73,8 +73,8 @@ function Expertise() {
 
                     <div className="skill">
                         <FontAwesomeIcon icon={faGears} size="3x" />
-                        <h3>Back-end et API</h3>
-                        <p>Je maîtrise le développement back-end avec des technologies telles que <strong>Node.js</strong>, <strong>Express</strong>, et des bases de données comme <strong>MongoDB</strong> et <strong>MySQL</strong>. J'intègre des <strong>API</strong> et gère l’authentification ainsi que la sécurité des données pour offrir des solutions robustes et évolutives.</p>
+                        <h3>Fonctionnalités sur mesure et sécurité des donnée</h3>
+                        <p>Derrière chaque site, il y a un moteur invisible : je développe ces fonctionnalités invisibles mais essentielles. Cela peut aller de la gestion de comptes utilisateurs à l’envoi de formulaires ou l’affichage d’informations en temps réel. Je fais en sorte que tout fonctionne bien ensemble, en toute sécurité, grâce à des outils solides pour gérer les bases de données et les échanges d'informations.</p>
                         <div className="flex-chips">
                             <span className="chip-title">Technologies:</span>
                             {labelsSecond.map((label, index) => (
@@ -85,8 +85,8 @@ function Expertise() {
 
                     <div className="skill">
                         <FontAwesomeIcon icon={faChartGantt} size="3x" />
-                        <h3>Organisation, SEO et Référencement</h3>
-                        <p>Je veille à la performance des sites avec une attention particulière au SEO et à l'optimisation pour le référencement naturel. J'utilise des outils comme <strong>Google Analytics</strong>,  <strong>Chrome DevTools</strong>, et mets en place des pipelines CI/CD pour assurer une gestion de projet efficace et une mise en production fluide.</p>
+                        <h3>Visibilité sur le web et bon fonctionnement</h3>
+                        <p>Un site bien fait, c’est aussi un site que l’on trouve facilement sur les moteurs de recherche. J’optimise mes projets pour qu’ils soient bien référencés (SEO) et rapides à charger. J’organise aussi mon travail avec des outils modernes pour que chaque étape – du développement à la mise en ligne – soit fluide, rapide, et bien structurée.</p>
                         <div className="flex-chips">
                             <span className="chip-title">Technologies:</span>
                             {labelsThird.map((label, index) => (
